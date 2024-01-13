2a6a833db670ea3ecc551f02daaf17f65e617aeb
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-01-13 18:47:40 +0700

    add reamur converter

--- script.js
+++ script.js
@@ -26,10 +26,39 @@ function submitForm() {
   // k to f
   else if (fromUnit === "k" && toUnit === "f") {
     result = (9 / 5) * (value - 273.15) + 32;
+  }
+  // re to c
+  else if (fromUnit === "re" && toUnit === "c") {
+    result = (5 / 4) * value;
+  }
+  // c to re
+  else if (fromUnit === "c" && toUnit === "re") {
+    result = (4 / 5) * value;
+  }
+  // re to f
+  else if (fromUnit === "re" && toUnit === "f") {
+    result = (9 / 4) * value + 32;
+  }
+  // f to re
+  else if (fromUnit === "f" && toUnit === "re") {
+    result = (4 / 9) * (value - 32);
+  }
+  // re to k
+  else if (fromUnit === "re" && toUnit === "k") {
+    result = (5 / 4) * value + 273.15;
+  }
+  // k to re
+  else if (fromUnit === "k" && toUnit === "re") {
+    result = (4 / 5) * (value - 273.15);
   } else {
     result = value;
   }
 
-  document.getElementById("result").innerText =
-    result.toFixed(2) + " º" + toUnit.toUpperCase();
+  if (toUnit === "k") {
+    document.getElementById("result").innerText =
+      result.toFixed(2) + " " + toUnit.toUpperCase();
+  } else {
+    document.getElementById("result").innerText =
+      result.toFixed(2) + " º" + toUnit.toUpperCase();
+  }
 }
